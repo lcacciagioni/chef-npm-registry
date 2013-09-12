a09d737aad4237c34e645a5090a1b0492a82307a
Cory Roloff <cory@brothersroloff.com>
2013-09-11 21:15:23 -0500

    Add specs.

--- spec/default_spec.rb
+++ spec/default_spec.rb
@@ -1,13 +1,21 @@
 require 'spec_helper'
 
 describe "#{File.basename(Dir.getwd)}::default" do
-	let(:runner) {
+	let(:chef_run) {
 		ChefSpec::ChefRunner.new(
 			platform: 'ubuntu', version: '12.04'
 		).converge("#{described_cookbook}::default")
 	}
 
-	it 'should' do
+	it 'installs curl' do
+		expect(chef_run).to install_package 'curl'
+	end
 
+	it 'kills beam' do
+		expect(chef_run).to execute_command('killall beam')
+	end
+
+	it 'restarts couchdb' do
+		expect(chef_run).to restart_service 'couchdb'
 	end
 end
